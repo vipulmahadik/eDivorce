a7ae498b638463a3781b1ee626a6c76eff9781a0
Wade Barnes <wade.barnes@shaw.ca>
2018-06-04 14:42:32 -0700

    EDIVORCE-45 - Set label based on environment.

--- edivorce/apps/core/context_processors.py
+++ edivorce/apps/core/context_processors.py
@@ -2,7 +2,10 @@ from django.conf import settings
 
 
 def settings_processor(request):
+    """ Add settings data to context to make visible in debug toolbar """
     return {
         'gtm_id': settings.GTM_ID,
-        'proxy_root_path': settings.FORCE_SCRIPT_NAME
+        'proxy_root_path': settings.FORCE_SCRIPT_NAME,
+        'deployment_environment': settings.ENVIRONMENT,
+        'show_debug': settings.ENVIRONMENT in ['localdev', 'dev', 'test', 'minishift']
     }
